da6d43cd7daa79e217c7340921a75924a6bc7de6
vvkrug <kraysir@yandex.ru>
2023-10-21 16:44:27 +0500

    Optimize dropdown toggle on input click
    Updated input onClick to open dropdown, added outside click to close.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.scss';
 import search from './assets/images/search.svg';
 import { Dropdown } from './components/Dropdown/Dropdown';
@@ -8,13 +8,30 @@ const App = () => {
   const [selectedItems, setSelectedItems] = useState([]);
 	const [isOpen, setIsOpen] = useState(false);
 	const [searchText, setSearchText] = useState('');
+  const dropdownRef = useRef(null);
+	// const inputRef = useRef(null); 
+
+	const handleClickOutside = (event) => {
+    // проверяем, не является ли элемент, по которому был произведен клик, полем ввода
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);  // закрываем выпадающий список, если клик произошел вне компонента и вне поля ввода
+    }
+  };
+
+	// Устанавливаем и очищаем обработчик событий
+	useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
 	const handleDropdownSelect = (items) => {
     setSelectedItems(items);
   };
 
   return (
-    <section className="location">
+    <section className="location" ref={dropdownRef}>
       <label className="location-label">Локация</label>
 			<div className={`location-search ${isOpen ? 'open' : ''}`}>
         <button className="location-button">
@@ -24,9 +41,10 @@ const App = () => {
 					type="text" 
 					placeholder={'ЖК, ОКРУГ, РАЙОН, МЕТРО'}
 					className="location-input" 
-					onClick={() => setIsOpen(!isOpen)}
+					onClick={() => setIsOpen(true)} // Открытие дропдауна при клике на инпут
 					value={searchText} // Связываем введенное значение
 					onChange={(e) => setSearchText(e.target.value)} // Обработчик изменений текста поиска
+					// ref={inputRef}
 				/>
 				<IconCircle 
 					type="filled" 
