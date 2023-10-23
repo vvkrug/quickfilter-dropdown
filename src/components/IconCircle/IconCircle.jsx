bb5562949841726a9c760d94722cc8d1636b4c7f
vvkrug <kraysir@yandex.ru>
2023-10-23 12:14:10 +0500

    Refactor IconCircle Component
    Added comments, utilized classNames library

--- src/components/IconCircle/IconCircle.jsx
+++ src/components/IconCircle/IconCircle.jsx
@@ -1,24 +1,28 @@
+import classNames from 'classnames'; // Библиотека для динамического присваивания классов
 import React from 'react';
 import tick from '../../assets/images/tick.svg';
 import './IconCircle.scss';
 
-export const IconCircle = ({ type, number, isSelected, onClick }) => {
+// Отображает круг с числом или галочкой, в зависимости от переданных свойств
+export const IconCircle = ({ number, isSelected, onClick }) => {
 	if (number === 0) {
-		return null; 
+		return null; // Не рендерим компонент, если не выбрано ни одного элемента
 	}
-  let style = "baseCircle";
 
-	if (isSelected || number !== undefined && <span>{number}</span>) {
-		type = 'filled';
-    style = number !== undefined ? "filledCircle" : "filledCircle check";
-  } else {
-    style = isSelected ? "filledCircle" : "outlinedCircle";
-  }
+	// Определяем нужно ли закрасить круг
+  const isFilled = isSelected || number !== undefined && number > 0;
+
+  const style = classNames({
+    baseCircle: true, // Базовый класс всегда применяется
+    filledCircle: isFilled, // Применяется если круг закрашен
+    outlinedCircle: !isFilled, // Применяется если круг не закрашен
+    check: isFilled && number === undefined,
+  });
 
   return (
     <div className={style} onClick={onClick}>
-      {type === 'filled' && number !== undefined && <span>{number}</span>}
-      {type === 'filled' && number === undefined && <img src={tick}/>}
+      {isFilled && number !== undefined && <span>{number}</span>} {/* Если круг закрашен и число определено, выводим это число */}
+      {isFilled && number === undefined && <img src={tick} alt="Selected" />} {/* Если круг закрашен, но число не определено, отображаем галочку */}
     </div>
   );
 }
